d559863172d05f821dc4a30cdfc5fca63713af45
Luke Stynes <stynesluke@gmail.com>
2022-08-23 11:43:18 +1200

    Add packet_builder which creates a bytearray of the packet data

--- client.py
+++ client.py
@@ -22,6 +22,34 @@ def client_init():
 
     return (req_type, addr, port)
 
+def request_packet_builder(requestType):
+    """Constructs a DT-Request packet"""
+
+    request_array = [0x497E, 0x0001, ]
+
+def packet_builder(data_array):
+    """Takes an array of all the packet numbers and converts it to a bytearray packet"""
+
+    composed_packet = bytearray()
+
+    #Goes through each inputted parameter and adjusts the numbers into bytes correctly
+    for parameter in data_array:
+        if parameter < 256:
+            composed_packet += parameter.to_bytes(1, byteorder="big")
+        elif parameter < 655356:
+            composed_packet += parameter.to_bytes(2, byteorder="big")
+        elif parameter < 16777216:
+            composed_packet += parameter.to_bytes(3, byteorder="big")
+        elif parameter < 4294967296:
+            composed_packet += parameter.to_bytes(4, byteorder="big")
+            
+    return composed_packet
+
+def make_request(init_values):
+    """Opens the UDP socket and sends the DT-Request packet"""
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 def main():
     """Main Function"""
