f75bd6536cf92bb05431e2407a9797a20b1197e1
Luke Stynes <stynesluke@gmail.com>
2022-08-23 11:35:11 +1200

    Add the initialisation for the client request

--- client.py
+++ client.py
@@ -1,12 +1,32 @@
 import socket
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def client_init():
+    """Get the initial info from the user for the setup"""
 
-msg = "Hello UDP Server, from client"
-client_sock.sendto(msg.encode('utf-8'), ('127.0.0.1', 12345))
+    req_type = input("Enter 'date' or 'time' depending what request you want: ")
+    if req_type != 'data' or req_type != 'time':
+        print("<ERROR: You must enter 'date' or 'time'>")
+        return -1
+    
+    addr = input("Enter the address of the server: ")
+    port = input("Enter the port of the server: ")
 
-data, addr = client_sock.recvfrom(4096)
-print("Server says")
-print(str(data))
+    if not port.isdigit():
+        print("<ERROR: Port number must be an integer>")
+        return -1
+    
+    port = int(port)
+    if port < 1024 or port > 64000:
+        print("<ERROR: Port number must be between 1024 and 64000>")
+        return -1
 
-client_sock.close()
+    return (req_type, addr, port)
+
+
+def main():
+    """Main Function"""
+    print("Datetime Client Running...")
+    init_values = client_init()
+
+if __name__ == "__main__":
+    main()
