088abc567ec738a3f8395fad127212e558cb5fb8
Luke Stynes <stynesluke@gmail.com>
2022-08-23 12:02:18 +1200

    Set up make_request and sends packet to server

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ def client_init():
     """Get the initial info from the user for the setup"""
 
     req_type = input("Enter 'date' or 'time' depending what request you want: ")
-    if req_type != 'data' or req_type != 'time':
+    if req_type != "date" and req_type != "time":
         print("<ERROR: You must enter 'date' or 'time'>")
         return -1
     
@@ -20,12 +20,7 @@ def client_init():
         print("<ERROR: Port number must be between 1024 and 64000>")
         return -1
 
-    return (req_type, addr, port)
-
-def request_packet_builder(requestType):
-    """Constructs a DT-Request packet"""
-
-    request_array = [0x497E, 0x0001, ]
+    return req_type, addr, port
 
 def packet_builder(data_array):
     """Takes an array of all the packet numbers and converts it to a bytearray packet"""
@@ -42,19 +37,35 @@ def packet_builder(data_array):
             composed_packet += parameter.to_bytes(3, byteorder="big")
         elif parameter < 4294967296:
             composed_packet += parameter.to_bytes(4, byteorder="big")
-            
+
     return composed_packet
 
-def make_request(init_values):
+def make_request(req_type, addr, port):
     """Opens the UDP socket and sends the DT-Request packet"""
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if req_type == 'date':
+        req = 0x0001
+    else:
+        req = 0x0002
+
+    request_array = [0x497E, 0x0001, req]
+    dt_request = packet_builder(request_array)
+
+    sock.sendto(dt_request, (addr, port))
+
+    data, srv_addr = sock.recvfrom(4096)
+    print("Server response")
+    print(str(data))
+
+    sock.close()
 
 
 def main():
     """Main Function"""
     print("Datetime Client Running...")
-    init_values = client_init()
+    req_type, addr, port = client_init()
+    make_request(req_type, addr, port)
 
 if __name__ == "__main__":
     main()
